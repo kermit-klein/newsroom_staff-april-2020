3a5fc35e7b463f461aee1d31c07727fc1a8f8cee
allezz <aerbay@gmail.com>
2020-05-30 18:47:46 +0200

    Add env variable

--- src/index.js
+++ src/index.js
@@ -8,10 +8,14 @@ import axios from "axios";
 import "./css/index.css";
 import { Provider } from "react-redux";
 import configureStore from "./state/store/configureStore";
-
-axios.defaults.baseURL = "http://localhost:3000/api";
+if (process.env.NODE_ENV == "production") {
+  axios.defaults.baseURL = process.env.REACT_APP_HEROKUURL;
+} else if (process.env.NODE_ENV == "development") {
+  axios.defaults.baseURL = process.env.REACT_APP_LOCALURL;
+}
+debugger;
 const store = configureStore();
-window.store = store
+window.store = store;
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
